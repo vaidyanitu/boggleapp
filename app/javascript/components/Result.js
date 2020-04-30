822f881f0a63e1bc6380b66483f6248e64446eaf
Nitu Vaidya <vaidyamenitu@gmail.com>
2020-05-01 02:26:10 +0545

    modify result component

--- app/javascript/components/Result.js
+++ app/javascript/components/Result.js
@@ -1,8 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 class Result extends React.Component {
+  totalscore = () => {
+    let sum = 0;
+    for (let num of this.props.validwords) {
+      sum = sum + num.length;
+    }
+    console.log("sum", sum);
+    return sum;
+  };
   render() {
-    return <React.Fragment>{this.props.validwords}</React.Fragment>;
+    return (
+      <React.Fragment>
+        <table>
+          <tbody>
+            <tr>
+              <th>Word</th>
+              <th>Score</th>
+            </tr>
+            {this.props.validwords.map((word, index) => (
+              <tr key={index}>
+                <td key={"a" + index}>{word}</td>
+                <td key={"b" + index}>{word.length}</td>
+              </tr>
+            ))}
+            <tr>
+              <td>Total Score</td>
+              <td>{this.totalscore()}</td>
+            </tr>
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
   }
 }
 
